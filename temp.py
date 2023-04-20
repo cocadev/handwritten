0a1b11ea427c9f4cdcdc8f5e6e2d11b73e82de25
Cocadev <40970351+cocadev@users.noreply.github.com>
2023-04-20 17:47:07 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,60 @@
 import matplotlib.pyplot as plt
 import torchvision
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-train = torchvision.datasets.MNIST('./data', train=True, download=True)
+# Load MNIST data
+train = torchvision.datasets.MNIST('data', train=True, download=True)
+test = torchvision.datasets.MNIST('data', train=True, download=True)
 
 fig, ax = plt.subplots(6, 6, sharex=True, sharey=True)
 for i in range(6):
     for j in range(6):
         ax[i][j].imshow(train.data[6*i+j], cmap="gray")
 plt.show()
+
+
+
+print(train.data.shape, train.targets.shape)
+print(test.data.shape, test.targets.shape)
+
+# each sample becomes a vector of values 0-1
+X_train = train.data.reshape(-1, 784).float() / 255.0
+y_train = train.targets
+X_test = test.data.reshape(-1, 784).float() / 255.0
+y_test = test.targets
+
+
+class Baseline(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(784, 784)
+        self.act1 = nn.ReLU()
+        self.layer2 = nn.Linear(784, 10)
+        
+    def forward(self, x):
+        x = self.act1(self.layer1(x))
+        x = self.layer2(x)
+        return x
+    
+model = Baseline()
+
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+loss_fn = nn.CrossEntropyLoss()
+loader = torch.utils.data.DataLoader(list(zip(X_train, y_train)), shuffle=True, batch_size=100)
+
+n_epochs = 10
+for epoch in range(n_epochs):
+    model.train()
+    for X_batch, y_batch in loader:
+        y_pred = model(X_batch)
+        loss = loss_fn(y_pred, y_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # Validation
+    model.eval()
+    y_pred = model(X_test)
+    acc = (torch.argmax(y_pred, 1) == y_test).float().mean()
+    print("Epoch %d: model accuracy %.2f%%" % (epoch, acc*100))
